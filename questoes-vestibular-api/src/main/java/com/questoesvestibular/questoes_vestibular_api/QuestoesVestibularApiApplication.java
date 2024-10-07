52c2be20fc0d291c07421ecc65c2462512ca2085
danilomassoni <massonidanilo@gmail.com>
2024-10-07 19:35:56 -0300

    Primeiro commit

---
+++ questoes-vestibular-api/src/main/java/com/questoesvestibular/questoes_vestibular_api/QuestoesVestibularApiApplication.java
@@ -0,0 +1,13 @@
+package com.questoesvestibular.questoes_vestibular_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestoesVestibularApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestoesVestibularApiApplication.class, args);
+	}
+
+}
---
+++ questoes-vestibular-api/src/test/java/com/questoesvestibular/questoes_vestibular_api/QuestoesVestibularApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questoesvestibular.questoes_vestibular_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestoesVestibularApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
